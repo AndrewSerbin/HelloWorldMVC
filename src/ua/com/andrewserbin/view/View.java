5324696873dbb4b5d1bd1ea9a38a2764349de468
AndrewSerbin <andrewserbin01@gmail.com>
2017-07-28 13:20:36 +0300

    Added view constants.

--- src/ua/com/andrewserbin/view/View.java
+++ src/ua/com/andrewserbin/view/View.java
@@ -5,11 +5,6 @@ package ua.com.andrewserbin.view;
  */
 public class View {
 
-    // Text's constants
-    public static final String INPUT_DATA = "Input data: ";
-    public static final String WRONG_INPUT = "Wrong input! Repeat please! ";
-    public static final String EXPECTED_STRING = "Expected string : ";
-
     /**
      * Prints message in console.
      * @param message
---
+++ src/ua/com/andrewserbin/view/ViewConstants.java
@@ -0,0 +1,12 @@
+package ua.com.andrewserbin.view;
+
+/**
+ * Created by admin on 28.07.2017.
+ */
+public class ViewConstants {
+
+    // Text's constants
+    public static final String INPUT_DATA = "Input data: ";
+    public static final String WRONG_INPUT = "Wrong input! Repeat please! ";
+    public static final String EXPECTED_STRING = "Expected string : ";
+}
